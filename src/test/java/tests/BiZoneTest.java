1259def0061a6a33b350813fac092b8b6130726b
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-06-28 21:39:40 +0300

    new support

--- src/test/java/tests/BiZoneTest.java
+++ src/test/java/tests/BiZoneTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import pages.*;
+
 
 
 @Tag("remote_test")
